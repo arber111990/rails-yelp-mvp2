494b05273e384abf7852116c238dfdc761da75d4
Arber Zyb <arber111990@gmail.com>
2019-07-11 13:53:20 +0100

    bugs solved on address

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,6 @@ class Restaurant < ApplicationRecord
 
   CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   validates :name, uniqueness: true, presence: true
-  validates :address, presence: true
+  # validates :address, presence: true
   validates :category, inclusion: { in: CATEGORIES }
 end
